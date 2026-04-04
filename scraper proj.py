2d83ba438d695ec8f249fa569c581bc58890e560
isntrid <ridn123321@gmail.com>
2026-04-04 22:47:37 +0100

    a

--- scraper proj.py
+++ scraper proj.py
@@ -3,30 +3,32 @@ from bs4 import BeautifulSoup
 
 
 try:
-    request = requests.get(r"https://pixelblade.fandom.com/wiki/Codes")
+    request = requests.get(r"https://pixelblade.fandom.com/wiki/Codes#How_to_Redeem_Codes_in_Pixel_Blade")
     html = request.text
 except Exception as e:
     print(f'An error occured, code: {e}')
     exit(1)
 
-
 soup = BeautifulSoup(html, 'html.parser')
+print(soup)
 tags = soup.find_all('b')
 data = []
 codes = []
 
 for tag in tags:
     data.append(tag.get_text(strip=True))
+    
 
 for d in data:
     if '(' and 'NEW' not in d:
         codes.append(d)
-    
+
 last_active = -1
 for i in range(len(codes) - 1, -1, -1):
     if codes[i] == '(Active)':
         last_active = i
         break
+    print()
 
 active = codes[9:last_active]
 active = [c for c in active if c != '(Active)']
